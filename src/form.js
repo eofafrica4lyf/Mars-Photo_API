d37a8dc1d5c9b6f4535811490d3bd0d1eaca0954
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-06-24 22:15:19 +0100

    Functionality completed.

---
+++ src/form.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Form (props){
+	return(
+			<>
+				<nav>
+            <span className="home">
+                Home
+            </span>
+					<span className="results">
+                Results
+            </span>
+					<span className="results">
+                Results
+            </span>
+				</nav>
+			</>
+	);
+}
+
+export default Form;
